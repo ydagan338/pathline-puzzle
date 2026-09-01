151a9ab77ca5535983bcc0dcebb2247a57125533
Yonathan Dagan <yonathandagan@Yonathans-MacBook-Air.local>
2026-09-01 15:14:56 -0400

    Republish GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  base: './',
+  base: '/pathline-puzzle/',
   plugins: [react()],
   test: {
     environment: 'node',
